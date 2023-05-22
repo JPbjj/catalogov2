abc1f41292c653a2d0f02048a74a288512ed7d06
JPbjj <jpsantosferrer@gmail.com>
2023-05-22 18:17:08 -0300

    PRIMEIRO COMMIT DA VERSÃO 2.0 DO CATALOGO DE PRODUTOS
    Segundo versionamento do projeto de catálogo de produtos, versão essa que conta com novas features e novo design.
    
    Contém:
    
    - Campo de pesquisa
    - Separação de produtos por categorias

---
+++ assets/js/script.js
@@ -0,0 +1,338 @@
+let produtos = {
+	data: [
+		{
+			produtoNome: "Balança",
+			categoria: "Diversos",
+			preco: "40",
+			image: "assets/images/balanca.png",
+		},
+		{
+			produtoNome: "BETA ALANINA 150G FTW SPORTS NUTRITION",
+			categoria: "Aminoacidos",
+			preco: "50",
+			image: "assets/images/betaalanina150.png",
+		},
+		{
+			produtoNome: "BETA ALANINA 300G FTW SPORTS NUTRITION",
+			categoria: "Aminoacidos",
+			preco: "80",
+			image: "assets/images/betaalanina300.png",
+		},
+		{
+			produtoNome: "TRIBULUS TERRESTRIS COM MACA PERUANA 90 CAPS",
+			categoria: "Diversos",
+			preco: "65",
+			image: "assets/images/bulgariantribulus.png",
+		},
+		{
+			produtoNome: "COLÁGENO TIPO II + MDK ALGY CARTRIL 60 CÁPSULAS",
+			categoria: "Diversos",
+			preco: "60",
+			image: "assets/images/colageno.png",
+		},
+		{
+			produtoNome: "GLUTAMINA DUX NUTRITION POTE 300G",
+			categoria: "Aminoacidos",
+			preco: "90",
+			image: "assets/images/glutaminadux.png",
+		},
+		{
+			produtoNome: "L-GLUTAMINE GUTAMINA PROBIÓTICA 300G",
+			categoria: "Aminoacidos",
+			preco: "80",
+			image: "assets/images/glutaminaprobiotica.png",
+		},
+		{
+			produtoNome: "GLUTAMAX GLUTAMINA VITAFOR POTE 300G",
+			categoria: "Aminoacidos",
+			preco: "90",
+			image: "assets/images/glutaminavitafor.png",
+		},
+		{
+			produtoNome: "GO! BODY WHEY PROTEIN 900G",
+			categoria: "Whey",
+			preco: "95",
+			image: "assets/images/gobody.png",
+		},
+		{
+			produtoNome: "GO! BODY WHEY PROTEIN WOMAN 900G",
+			categoria: "Whey",
+			preco: "95",
+			image: "assets/images/gobodywoman.png",
+		},
+		{
+			produtoNome: "L-CARNITINA 2000 NEW MILLEN 500 ML",
+			categoria: "Aminoacidos",
+			preco: "60",
+			image: "assets/images/lcarnitta.png",
+		},
+		{
+			produtoNome: "DELICIOUS MASS HIPERCALÓRICO FTW 3KG",
+			categoria: "Diversos",
+			preco: "80",
+			image: "assets/images/massa.png",
+		},
+		{
+			produtoNome: "ÔMEGA MEGA DHA 120 CÁPSULAS VITAFOR",
+			categoria: "Diversos",
+			preco: "170",
+			image: "assets/images/megadha.png",
+		},
+		{
+			produtoNome: "MULTIVITAMICO MEGA MEN GNC 180 CAPSULAS",
+			categoria: "Diversos",
+			preco: "335",
+			image: "assets/images/megamengnc.png",
+		},
+		{
+			produtoNome: "MELATONINA 3 MG 100 TABLETES KN NUTRITION",
+			categoria: "Diversos",
+			preco: "75",
+			image: "assets/images/melatoninakn.png",
+		},
+		{
+			produtoNome: "NITRO WHEY 3W COCO/BAUNILHA 900G",
+			categoria: "Whey",
+			preco: "95",
+			image: "assets/images/nitrowhey3w.png",
+		},
+		{
+			produtoNome: "PASTA DE AMENDOIM VITAPOWER AIR INTEGRAL SUPER CREAMY 80G",
+			categoria: "Diversos",
+			preco: "15",
+			image: "assets/images/pastaamendoin80g.png",
+		},
+		{
+			produtoNome: "VITAPOWER PASTA DE AMENDOIM INTEGRAL PRESS CREAM 1KG",
+			categoria: "Diversos",
+			preco: "55",
+			image: "assets/images/pastaintegral.png",
+		},
+		{
+			produtoNome: "PASTA DE AMENDOIM SWEET BOMBOM DE AVELÃ 500G - POWER ONE",
+			categoria: "Diversos",
+			preco: "35",
+			image: "assets/images/pastapower.png",
+		},
+		{
+			produtoNome: "PRÉ TREINO DINAMIC PRÉ WORKOUT TESTODROL 3D 300 G",
+			categoria: "Diversos",
+			preco: "40",
+			image: "assets/images/pretreino.png",
+		},
+		{
+			produtoNome: "PRO UP WHEY BEST BLEND CHOCOLATE / BAUNILHA / MORANGO 900G",
+			categoria: "Whey",
+			preco: "95",
+			image: "assets/images/proupwhey.png",
+		},
+		{
+			produtoNome: "REVOLTS NUTRITION REVOWHEY CONCENTRATE 1.8KG",
+			categoria: "Whey",
+			preco: "95",
+			image: "assets/images/revowhey.png",
+		},
+		{
+			produtoNome: "VITAMINA C PROVITAM 10 COMPRIMIDOS",
+			categoria: "Diversos",
+			preco: "10",
+			image: "assets/images/provitam.png",
+		},
+		{
+			produtoNome: "SEKKA ABDOMEN CONTROL RED SERIES 30 TABLETES",
+			categoria: "Termogenicos",
+			preco: "30",
+			image: "assets/images/sekkablack.png",
+		},
+		{
+			produtoNome: "SEKKA ABDOMEN NIGHT 30 TABLETES - RED SERIES",
+			categoria: "Termogenicos",
+			preco: "35",
+			image: "assets/images/sekkanight.png",
+		},
+		{
+			produtoNome: "SEKKA ABDOMEN CONTROL RED SERIES 30 TABLETES",
+			categoria: "Termogenicos",
+			preco: "25",
+			image: "assets/images/sekkared.png",
+		},
+		{
+			produtoNome: "SEKKA ABDOMEN CONTROL RED SERIES 30 TABLETES",
+			categoria: "Termogenicos",
+			preco: "20",
+			image: "assets/images/sekkayellow.png",
+		},
+		{
+			produtoNome: "SPIRULINA 500MG 100 CÁPSULAS DENATURE",
+			categoria: "Diversos",
+			preco: "40",
+			image: "assets/images/spirulina_denature.png",
+		},
+		{
+			produtoNome: "SUPER SEKKA ABDÔMEN NIGHT 30 TABLETES",
+			categoria: "Termogenicos",
+			preco: "45",
+			image: "assets/images/supersekka.png",
+		},
+		{
+			produtoNome: "SYNTHESIZE CREATINA MONOHIDRATADA 300G",
+			categoria: "Aminoacidos",
+			preco: "150",
+			image: "assets/images/synthesizecreatine.png",
+		},
+		{
+			produtoNome: "LIPO 6 BLACK ULTRA CONCENTRADO 60 CAPSULAS NUTREX",
+			categoria: "Termogenicos",
+			preco: "190",
+			image: "assets/images/termogenicolipo6.png",
+		},
+		{
+			produtoNome: "BLACK VIPER TERMOGÊNICO 90 CÁPSULAS",
+			categoria: "Termogenicos",
+			preco: "220",
+			image: "assets/images/termogenicoviper.png",
+		},
+		{
+			produtoNome: "BODY ACTION THERMO ABDOMEN CAFEÍNA 240MG",
+			categoria: "Termogenicos",
+			preco: "220",
+			image: "assets/images/thermoabdomem.png",
+		},
+		{
+			produtoNome: "CAFÉ TERMOGÊNICO THREE COFFEE 225G",
+			categoria: "Termogenicos",
+			preco: "85",
+			image: "assets/images/threecoffee.png",
+		},
+		{
+			produtoNome: "TRIBULUS TERRESTRIS BP NUTRITION 90 CAPS",
+			categoria: "Diversos",
+			preco: "55",
+			image: "assets/images/tribulusdevastet.png",
+		},
+		{
+			produtoNome:
+				"TRIBULUS TERRESTRIS STAR LABS MACA PERUANA 2G / 90% SAPONINS",
+			categoria: "Diversos",
+			preco: "75",
+			image: "assets/images/tribulus.png",
+		},
+		{
+			produtoNome: "WHEY PROTEIN BLACK SKULL 'CAVEIRA PRETA' 900G",
+			categoria: "Whey",
+			preco: "130",
+			image: "assets/images/wheyblackskull.png",
+		},
+		{
+			produtoNome: "WHEY PROTEIN BLACK SKULL 'CAVEIRA PRETA' TURBO 907G",
+			categoria: "Whey",
+			preco: "120",
+			image: "assets/images/wheyblackskullturbo.png",
+		},
+		{
+			produtoNome: "WHEY PROTEIN CONCENTRADO DUX POTE 900G",
+			categoria: "Whey",
+			preco: "180",
+			image: "assets/images/wheydux2concentrado.png",
+		},
+		{
+			produtoNome: "WHEY PROTEIN ISOLADO DUX POTE 900G",
+			categoria: "Whey",
+			preco: "250",
+			image: "assets/images/wheyduxisolado.png",
+		},
+		{
+			produtoNome: "WHEY PROTEIN DUX ISOLADO SACO 1.8KG",
+			categoria: "Whey",
+			preco: "400",
+			image: "assets/images/wheyduxisolado1_8kg.png",
+		},
+		{
+			produtoNome:
+				"WHEY PROTEIN 100% WHEY GOLD STANDARD 907G OPTIMUM NUTRITION",
+			categoria: "Whey",
+			preco: "250",
+			image: "assets/images/wheygold.png",
+		},
+		{
+			produtoNome: "WHEY GOURMET ISOLATE 900G IOGURTE DE COCO",
+			categoria: "Whey",
+			preco: "120",
+			image: "assets/images/wheygourmetcoco.png",
+		},
+		{
+			produtoNome: "WWHEY GOURMET ISOLATE 900G VANILLA ICE CREAM",
+			categoria: "Whey",
+			preco: "250",
+			image: "assets/images/wheygourmetvanilla.png",
+		},
+	],
+};
+
+for (let i of produtos.data) {
+	//criando cards
+	let card = document.createElement("div");
+	//cards recebem categorias que estao no objeto
+	card.classList.add("card", i.categoria);
+	//image div
+	let imgContainer = document.createElement("div");
+	imgContainer.classList.add("image-container");
+	let image = document.createElement("img");
+	image.setAttribute("src", i.image);
+	imgContainer.appendChild(image);
+	card.appendChild(imgContainer);
+	//container
+	let container = document.createElement("div");
+	container.classList.add("container_descricao-produto");
+	//nome do produto
+	let nome = document.createElement("h5");
+	nome.classList.add("nome_produto");
+	nome.innerText = i.produtoNome.toUpperCase();
+	container.appendChild(nome);
+	//preço
+	let preco = document.createElement("h6");
+	preco.classList.add("preco_produto");
+	preco.innerText = "R$" + i.preco + ",00";
+	container.appendChild(preco);
+
+	card.appendChild(container);
+	document.getElementById("produtos").appendChild(card);
+}
+function filtrarProduto(valor) {
+	let botoes = document.querySelectorAll(".botao_valor");
+	botoes.forEach((botao) => {
+		if (valor.toUpperCase() == botao.innerText.toUpperCase()) {
+			botao.classList.add("ativo");
+		} else {
+			botao.classList.remove("ativo");
+		}
+	});
+
+	let elementos = document.querySelectorAll(".card");
+	elementos.forEach((elemento) => {
+		if (valor == "todos") {
+			elemento.classList.remove("hide");
+		} else {
+			if (elemento.classList.contains(valor)) {
+				elemento.classList.remove("hide");
+			} else {
+				elemento.classList.add("hide");
+			}
+		}
+	});
+}
+
+//Botao pesquisa ao clicar
+document.getElementById("idbotaopesquisa").addEventListener("click", () => {
+	let inputPesquisa = document.getElementById("input-pesquisa").value;
+	let elementos = document.querySelectorAll(".nome_produto");
+	let cards = document.querySelectorAll(".card");
+
+	elementos.forEach((elemento, index) => {
+		if (elemento.innerText.includes(inputPesquisa.toUpperCase())) {
+			cards[index].classList.remove("hide");
+		} else {
+			cards[index].classList.add("hide");
+		}
+	});
+});
